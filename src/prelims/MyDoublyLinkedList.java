47c2870a1817013ae78eba6b7d15c98205daa559
Atlas-jjs <sabogjazreiljaron@gmail.com>
2024-09-18 19:06:32 +0800

    Added javadocs to MySinglyLinkedList and MyDoublyLinkedList

--- src/prelims/MyDoublyLinkedList.java
+++ src/prelims/MyDoublyLinkedList.java
@@ -1,25 +1,62 @@
 package prelims;
 
+import java.util.NoSuchElementException;
+
+/*
+  Name: Sabog, Jazreil Jaron V.
+  Date: September 18, 2024
+  Class Code: 9356B CS 211L
+  Individual Prelim Project
+ */
 public class MyDoublyLinkedList<T> extends DoublyLinkedListNode<T>{
-    private DoublyLinkedListNode<T> head, tail;
+    /**
+     * Head Node Attribute
+     */
+    private DoublyLinkedListNode<T> head;
+
+    /**
+     * Tail Node Attribute
+     */
+    private DoublyLinkedListNode<T> tail;
+
+    /**
+     * Default Constructor
+     */
     public MyDoublyLinkedList() {
         head = null;
         tail = null;
     }
 
+    /**
+     * Constructs a doubly linked list with the specified head and tail nodes
+     * @param head the head node of the list
+     * @param tail the tail node of the list
+     */
     public MyDoublyLinkedList(DoublyLinkedListNode<T> head, DoublyLinkedListNode<T> tail) {
         this.head = head;
         this.tail = tail;
     }
 
+    /**
+     * Gets the head node of the list
+     * @return the head node
+     */
     public DoublyLinkedListNode<T> getHead() {
         return head;
     }
 
+    /**
+     * Gets the tail node of the list
+     * @return the tail node
+     */
     public DoublyLinkedListNode<T> getTail() {
         return tail;
     }
 
+    /**
+     * Inserts a node at the start of the list
+     * @param data the data to inserted to the new node to be inserted at the start of the list
+     */
     public void insertStart(T data) {
         DoublyLinkedListNode<T> newNode = new DoublyLinkedListNode<>(data);
         DoublyLinkedListNode<T> currentNode = head;
@@ -33,6 +70,10 @@ public class MyDoublyLinkedList<T> extends DoublyLinkedListNode<T>{
         if (tail == null) tail = newNode;
     }
 
+    /**
+     * Inserts a node at the end of the list
+     * @param data the data to inserted to the new node to be inserted at the end of the list
+     */
     public void insertEnd(T data) {
         DoublyLinkedListNode<T> newNode = new DoublyLinkedListNode<>(data);
         DoublyLinkedListNode<T> currentNode = tail;
@@ -46,10 +87,14 @@ public class MyDoublyLinkedList<T> extends DoublyLinkedListNode<T>{
         if (head == null) head = newNode;
     }
 
+    /**
+     * Removes the tail node from the list
+     * @throws NoSuchElementException if the list is empty
+     */
     public void removeEnd() {
         DoublyLinkedListNode<T> nodeToRemove = tail;
 
-        if (nodeToRemove == null) throw new NullPointerException();
+        if (nodeToRemove == null) throw new NoSuchElementException("The List is empty");
 
         tail = nodeToRemove.getPrevious();
 
@@ -58,10 +103,14 @@ public class MyDoublyLinkedList<T> extends DoublyLinkedListNode<T>{
         if (nodeToRemove == head) removeStart();
     }
 
+    /**
+     * Removes the head node from the list
+     * @throws NoSuchElementException if the list is empty
+     */
     public void removeStart() {
         DoublyLinkedListNode<T> nodeToRemove = head;
 
-        if (nodeToRemove == null) throw new NullPointerException();
+        if (nodeToRemove == null) throw new NoSuchElementException("The List is empty");
 
         head = nodeToRemove.getNext();
 
@@ -70,6 +119,10 @@ public class MyDoublyLinkedList<T> extends DoublyLinkedListNode<T>{
         if (nodeToRemove == tail) removeEnd();
     }
 
+    /**
+     * Removes the first occurrence that matches the given data from the list
+     * @param data the data to be removed
+     */
     public void removeByData(T data) {
         DoublyLinkedListNode<T> nodeToRemove = null;
         DoublyLinkedListNode<T> currentNode = head;
@@ -96,10 +149,17 @@ public class MyDoublyLinkedList<T> extends DoublyLinkedListNode<T>{
         }
     }
 
+    /**
+     * Checks if the list is empty
+     * @return true if the list is empty, otherwise false
+     */
     public boolean isEmpty() {
         return head == null;
     }
 
+    /**
+     * Returns a string representation of the list
+     */
     @Override
     public String toString() {
         if (head == null) return "List is Empty";
@@ -114,6 +174,3 @@ public class MyDoublyLinkedList<T> extends DoublyLinkedListNode<T>{
         return result;
     }
 }
-
-// REFERENCE
-// https://www.codecademy.com/learn/linear-data-structures-java/modules/doubly-linked-lists-java/cheatsheet
